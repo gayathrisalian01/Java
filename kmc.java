bc540772b5cac51b168ceae1e8cb215ff1c621a0
Gayathri P Salian <141249949+gayathrisalian01@users.noreply.github.com>
2024-04-26 23:26:06 +0530

    Add files via upload

---
+++ kmc.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class newBornbaby
+{
+	String babyID="",weight="",mothername="";
+	Date d=new Date();
+	newBornBaby(String id,String wt,String mname,char g)
+	{
+		babyID=id;
+		weight=wt;
+		mothername=mname;
+		gender=g;
+	}
+	void dates()
+	{
+		date bcg;
+		System.out.println(bcg);
+	}
+
+}
+class kmc
+{
+public static void main(String args[])
+{
+}
+}
