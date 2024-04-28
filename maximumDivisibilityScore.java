0c9a608fe3a9f6b8f016446f2e9c69d896702a66
Gayathri P Salian <141249949+gayathrisalian01@users.noreply.github.com>
2024-04-28 20:54:01 +0530

    Create maximumDivisibilityScore.java

---
+++ maximumDivisibilityScore.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxDivScore(int[] nums, int[] divisors) {
+        Arrays.sort(divisors);
+        int max = 0, res = divisors[0];
+        for(int i=0; i<divisors.length; i++){
+            int cnt = 0;
+            for(int j=0; j<nums.length; j++){
+                if(nums[j] % divisors[i] == 0)  cnt++;
+            }
+            if(cnt > max){
+                max = cnt;
+                res = divisors[i];
+            }
+        }
+        return res;
+    }
+}
